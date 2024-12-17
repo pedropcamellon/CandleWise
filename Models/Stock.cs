c29389fde39d131e09272046059aa82e961a6592
Pedro P. Camellon <pedropcamellon@gmail.com>
2024-12-17 14:52:34 -0500

    Initial

---
+++ Models/Stock.cs
@@ -0,0 +1,27 @@
+﻿namespace CandleWise.Models
+{
+    public class Stock
+    {
+        //public Stock(string name, float priceNow)
+        //{
+        //    Name = name;
+        //    PriceNow = priceNow;
+        //}
+
+        public long Id { get; set; }
+
+        public string Symbol { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+
+        public decimal Price { get; set; } = 0;
+
+        //public float PriceOpen { get; set; } = 0;
+
+        //public float PriceClose { get; set; } = 0;
+
+        //public float PriceHigh { get; set; } = 0;
+
+        //public float PriceLow { get; set; } = 0;
+    }
+}
