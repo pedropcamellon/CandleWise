5831d19d7d4813df6999a312182f5b9f1e4de204
Pedro P. Camellon <pedropcamellon@gmail.com>
2025-08-02 11:27:51 -0400

    feat: add Azure authentication variables for Terraform configuration

--- infra/variables.tf
+++ infra/variables.tf
@@ -38,3 +38,21 @@ variable "resource_group_name" {
   type        = string
   default     = ""
 }
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+  sensitive   = true
+}
